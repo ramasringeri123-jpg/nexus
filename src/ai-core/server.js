d00154e196e74a1ff304e4eee38d43998d2a7b15
Sriram <ramasringeri123@gmail.com>
2026-03-11 18:42:44 +0530

    Update

--- src/ai-core/server.js
+++ src/ai-core/server.js
@@ -14,7 +14,8 @@ import mongoose from "mongoose";
 // --- MODELS (Fixed case sensitivity) ---
 import User from "./models/User.js";
 import Chat from "./models/chat.js"; // Fixed: Capital C to match filename
-import Reel from "./models/Reel.js"; 
+// Change this line in your src/ai-core/server.js
+import Reel from "./models/Reel.js"; // Use lowercase 'r' here
 
 import { generateImage } from "./imageGenerator.js";
 import { generateVoice } from "./voiceGenerator.js";
